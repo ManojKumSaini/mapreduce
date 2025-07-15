fe48df2d1cbf6715152571b751d12e426b7245f1
Manoj Kumar <manoj2000saini@example.com>
2025-07-15 15:49:31 +0200

    Task 4

--- mapper.py
+++ mapper.py
@@ -1,9 +1,11 @@
 import sys
 
+allowed = {"Computers", "Cameras", "Video Games"}
+
 for line in sys.stdin:
     fields = line.strip().split(',')
-    if len(fields) != 6:
-        raise ValueError(f"Expected 6 elements, got {len(fields)}: {line.strip()}")
     category = fields[1]
+    if category not in allowed:
+        continue
     amount = float(fields[3])
     print(f"{category}\t{amount}")
