51b8836ab4910dd6a43ecea40b17e5baaf8fb8dc
Manoj Kumar <manoj2000saini@example.com>
2025-07-15 15:46:23 +0200

    1st exercise

--- mapper.py
+++ mapper.py
@@ -28,17 +28,8 @@ import sys
 
 # For each new line in the standard input (stdin) 
 for line in sys.stdin:
+    fields = line.strip().split(',')
+    category = fields[1]
+    amount = float(fields[3])
+    print(f"{category}\t{amount}")
 
-    # split the line at the tabulator ("\t")
-    # strip removes whitespaces and new lines at the beginning and end of the line
-    # the result is a tuple with 6 elements
-    data = line.strip().split("\t")
-
-    # store the 6 elements of the tuple in seperate variables
-    date, time, item, category, sales, payment = data
-
-    # Write the key-value combination to standard output (stdout)
-    # Key is the payment, value is the sales     
-    # With a tab (\t) between key and value
-    # New line \n means new record
-    sys.stdout.write("{0}\t{1}\n".format(payment, sales))
