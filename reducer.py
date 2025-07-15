91e3df64f31c57411274619bc6615f8590255f92
Manoj Kumar <manoj2000saini@example.com>
2025-07-15 15:51:36 +0200

    6

--- reducer.py
+++ reducer.py
@@ -1,18 +1,23 @@
-#!/usr/bin/env python3
 import sys
 
 current_category = None
+total_amount = 0.0
 count = 0
 
 for line in sys.stdin:
-    category, value = line.strip().split('\t')
+    category, amount_str = line.strip().split('\t')
+    amount = float(amount_str)
     if current_category == category:
+        total_amount += amount
         count += 1
     else:
-        if current_category and count > 114:
-            print(f"{current_category}\t{count}")
+        if current_category:
+            avg = total_amount / count
+            print(f"{current_category}\t{avg}")
         current_category = category
+        total_amount = amount
         count = 1
 
-if current_category and count > 114:
-    print(f"{current_category}\t{count}")
+if current_category:
+    avg = total_amount / count
+    print(f"{current_category}\t{avg}")
